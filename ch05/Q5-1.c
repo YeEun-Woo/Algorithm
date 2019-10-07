28ba7cb9178de1d08bec42dcef93db95c3da78a6
예은 우 <co1869@cu.ac.kr>
2019-10-07 14:03:46 +0900

    add

--- ch05/Q5-1.c
+++ ch05/Q5-1.c
@@ -1,24 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 
 int factorial(int n)
 {
-	int fact = 1;
-
-	while (n > 1)
-		fact *= n--;
-	return fact;
+	int sum = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		sum *= i; //sum=sum*i;
+	}
+	return sum;
 }
 
 int main(void)
 {
-	int x;
+	int sum;
 
-	printf("정수를 입력하세요：");
-	scanf("%d", &x);
-
-	printf("%d의 factorial은 %d입니다.\n", x, factorial(x));
+	printf("정수를 입력하세요: ");
+	scanf_s("%d", &sum);
+	printf("%d의 factorial은 %d입니다.\n", sum, factorial(sum));
 
 	return 0;
 }
-
