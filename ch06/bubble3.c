a53ac042b0b0b74d7076156859be204fb6d73099
예은 우 <co1869@cu.ac.kr>
2019-10-14 13:32:26 +0900

    add

--- ch06/bubble3.c
+++ ch06/bubble3.c
@@ -6,11 +6,16 @@
 
 void bubble(int a[], int n)
 {
-	int i, j;
-	for (i = 0; i < n - 1; i++) {
-		for (j = n - 1; j > i; j--)
-			if (a[j - i] > a[j])
-			 swap(int, a[j - i], a[j]);
+	int k = 0;
+	while (k < n - 1) {
+		int j;
+		int last = n - 1;
+		for (j = n - 1; j > k; j--)
+			if (a[j - 1] > a[j]) {
+				swap(int, a[j - 1], a[j]);
+				last = j;
+			}
+		k = last;
 	}
 }
 
